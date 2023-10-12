de09b5924c56b3e74ae020459772d8235f2485a4
NU-COSMOS <nucosmos1234@gmail.com>
2023-10-13 01:03:27 +0900

    コメントや型ヒント追記

--- src/deck.py
+++ src/deck.py
@@ -1,24 +1,27 @@
 import json
 from attrdict import AttrDict
-from src.card import Card, Monster, Energy
+from src.card import Monster, Energy
+
 
 class Deck:
     def __init__(self, deck_path: str) -> None:
         self.deck_path = deck_path
         self.deck = self.load_deck()
 
-        for card in self.deck:
-            print(card.name, card.type)
-
-    def load_deck(self) -> list[Card]:
+    def load_deck(self) -> list[Monster | Energy]:
+        """デッキの読み込み"""
         with open(self.deck_path) as f:
             deck_dict = AttrDict(json.load(f))
         
+        # カードの読み込み
         deck = []
         for card in deck_dict.cards:
             if card.type == "monster":
                 deck.append(Monster(card))
             elif card.type == "energy":
                 deck.append(Energy(card))
+
+        # デッキ所有者
+        self.owner = deck_dict.owner
 
         return deck
